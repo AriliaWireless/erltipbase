8029885367beb599aff9f1ff672075e2ad8455fb
stephb9959 <stephane.bourque@gmail.com>
2022-08-31 22:35:48 -0700

    Initial commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+PROJECT = erltipbase
+PROJECT_DESCRIPTION = New project
+PROJECT_VERSION = 0.1.0
+
+include erlang.mk
---
+++ src/erltipbase_app.erl
@@ -0,0 +1,11 @@
+-module(erltipbase_app).
+-behaviour(application).
+
+-export([start/2]).
+-export([stop/1]).
+
+start(_Type, _Args) ->
+	erltipbase_sup:start_link().
+
+stop(_State) ->
+	ok.

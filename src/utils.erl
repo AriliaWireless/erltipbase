c37f2aefeb7815c6cf290cffa5d2dd01a6906dad
stephb9959 <stephane.bourque@gmail.com>
2023-02-01 08:35:12 -0800

    Initial commit:
    Signed-off-by: stephb9959 <stephane.bourque@gmail.com>

--- src/utils.erl
+++ src/utils.erl
@@ -38,10 +38,11 @@ get_app_name() ->
 number_of_cpus() ->
 	length(cpu_sup:util([detailed,per_cpu])).
 
-add_cors(Req0, _Methods) ->
+add_cors(Req0, Methods) ->
 	Req1 = cowboy_req:set_resp_header(<<"access-control-allow-credentials">>, <<"true">>, Req0),
 	Req2 = cowboy_req:set_resp_header(<<"access-control-request-headers">>, <<"*">>, Req1),
 	Req3 = cowboy_req:set_resp_header(<<"access-control-allow-origin">>, <<"*">>, Req2),
 	Req4 = cowboy_req:set_resp_header(<<"vary">>, <<"origin, accept-encoding">>, Req3),
-	Req5 = cowboy_req:set_resp_header(<<"access-control-allow-methods">>, <<"*">>, Req4),
+%%	Req5 = cowboy_req:set_resp_header(<<"access-control-allow-methods">>, <<"*">>, Req4),
+	Req5 = cowboy_req:set_resp_header(<<"access-control-allow-methods">>, Methods, Req4),
 	cowboy_req:set_resp_header(<<"access-control-max-age">>, <<"20">>, Req5).

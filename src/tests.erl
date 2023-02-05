11bedfc637ce33b32d4a23e2443bd186100ccc12
stephb9959 <stephane.bourque@gmail.com>
2023-02-05 13:34:09 -0800

    Initial commit:
    Signed-off-by: stephb9959 <stephane.bourque@gmail.com>

---
+++ src/tests.erl
@@ -0,0 +1,16 @@
+%%%-------------------------------------------------------------------
+%%% @author stephb
+%%% @copyright (C) 2023, Arilia Wireless Inc.
+%%% @doc
+%%%
+%%% @end
+%%% Created : 05. Feb 2023 1:33 p.m.
+%%%-------------------------------------------------------------------
+-module(tests).
+-author("stephb").
+
+%% API
+-export([ws1/0]).
+
+ws1() ->
+	ws_user_registry:send_frame(<<"script.runner@arilia.com">>,{text,<<"Hello">>}).
